fdd667f60e1e9c6644cae81e880698f03e324197
Tobi <owillia2@umbc.edu>
2023-12-01 01:23:03 -0500

    read functionality implemented

--- part1/magic8ball.c
+++ part1/magic8ball.c
@@ -4,6 +4,8 @@
 #include <linux/random.h>
 #include <linux/init.h>
 #include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/prandom.h>
 
 MODULE_LICENSE("GPL");
 
@@ -61,19 +63,25 @@ static int device_close(struct inode *inode, struct file *file) {
 }
 
 static ssize_t device_read(struct file *file, char __user *buff, size_t len, loff_t *offset){
-    unsigned int rand_num = prandom_u32();
-    rand_num %= ARRAY_SIZE(strings);
+    unsigned int rand_num = prandom_u32(); //generate random number
+    size_t str_len;
+    
+    if(*offset > 0){
+    	return 0; //EOF
+    }
+    
+    rand_num %= ARRAY_SIZE(strings); //truncate random number to bounds of array
 
-    size_t str_len = strlen(strings[rand_num]);
-    if((str_len) > len){
+    str_len = strlen(strings[rand_num]); //get length of chosen random string
+    if((str_len) > len){ //make sure to not write more than length of provided buffer
         str_len = len;
     }
 
-    if(copy_to_user(buff, string[rand_num], str_len)){
+    if(copy_to_user(buff, strings[rand_num], str_len)){ //copy string to user space buffer, check for error
         return -EFAULT;
     }
 
-    //*offset += str_len;
+    *offset += str_len;
     return str_len;
 }
 
@@ -92,13 +100,8 @@ static int __init magic8ball_init(void) {
 } 
 
 static void __exit magic8ball_exit(void) {
-    int ret = misc_deregister(&magic8ball);
-    if(ret < 0){
-        printk(KERN_ERR "Magic8Ball module failed to unload\n");
-        return ret;
-    }
-    printk(KERN_ALERT "Magic8Ball module unloaded successfully\n");
-    return 0;
+    misc_deregister(&magic8ball);
+    printk(KERN_ALERT "Magic8Ball module unloaded\n");
 }
 
 module_init(magic8ball_init);
