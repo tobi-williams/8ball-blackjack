6b5fe573b702bacad65aa97245b94e5a6dfa41fa
Tobi <owillia2@umbc.edu>
2023-12-02 19:12:51 -0500

    shuffle helper function and minor modifications to skeleton code

--- part2/blackjack.c
+++ part2/blackjack.c
@@ -13,7 +13,8 @@ static int device_open(struct inode *inode, struct file *file);
 static int device_close(struct inode *inode, struct file *file);
 static ssize_t device_read(struct file *file, char __user *buff, size_t len, loff_t *offset);
 static ssize_t device_write(struct file *file, const char __user *buff, size_t len, loff_t *offset);
-static void shuffle(struct card_deck *deck);
+static void shuffle(int card_numbers[]);
+static void reset(int card_numbers[]);
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
@@ -31,75 +32,78 @@ static struct miscdevice blackjack = {
 };
 
 
+static const char *card_deck[] = {
+	"Ace of Spades\n",
+	"2 of Spades\n",
+	"3 of Spades\n",
+	"4 of Spades\n",
+	"5 of Spades\n",
+	"6 of Spades\n",
+	"7 of Spades\n",
+	"8 of Spades\n",
+	"9 of Spades\n",
+	"10 of Spades\n",
+	"Jack of Spades\n",
+	"Queen of Spades\n",
+	"King of Spades\n",
+	"Ace of Hearts\n",
+	"2 of Hearts\n",
+	"3 of Hearts\n",
+	"4 of Hearts\n",
+	"5 of Hearts\n",
+	"6 of Hearts\n",
+	"7 of Hearts\n",
+	"8 of Hearts\n",
+	"9 of Hearts\n",
+	"10 of Hearts\n",
+	"Jack of Hearts\n",
+	"Queen of Hearts\n",
+	"King of Hearts\n",
+	"Ace of Diamonds\n",
+	"2 of Diamonds\n",
+	"3 of Diamonds\n",
+	"4 of Diamonds\n",
+	"5 of Diamonds\n",
+	"6 of Diamonds\n",
+	"7 of Diamonds\n",
+	"8 of Diamonds\n",
+	"9 of Diamonds\n",
+	"10 of Diamonds\n",
+	"Jack of Diamonds\n",
+	"Queen of Diamonds\n",
+	"King of Diamonds\n",
+	"Ace of Clubs\n",
+	"2 of Clubs\n",
+	"3 of Clubs\n",
+	"4 of Clubs\n",
+	"5 of Clubs\n",
+	"6 of Clubs\n",
+	"7 of Clubs\n",
+	"8 of Clubs\n",
+	"9 of Clubs\n",
+	"10 of Clubs\n",
+	"Jack of Clubs\n",
+	"Queen of Clubs\n",
+	"King of Clubs\n"
+};
 
-struct deck {
-    const char *card_names[] = {
-        "Ace of Spades\n",
-        "2 of Spades\n",
-        "3 of Spades\n",
-        "4 of Spades\n",
-        "5 of Spades\n",
-        "6 of Spades\n",
-        "7 of Spades\n",
-        "8 of Spades\n",
-        "9 of Spades\n",
-        "10 of Spades\n",
-        "Jack of Spades\n",
-        "Queen of Spades\n",
-        "King of Spades\n",
-        "Ace of Hearts\n",
-        "2 of Hearts\n",
-        "3 of Hearts\n",
-        "4 of Hearts\n",
-        "5 of Hearts\n",
-        "6 of Hearts\n",
-        "7 of Hearts\n",
-        "8 of Hearts\n",
-        "9 of Hearts\n",
-        "10 of Hearts\n",
-        "Jack of Hearts\n",
-        "Queen of Hearts\n",
-        "King of Hearts\n",
-        "Ace of Diamonds\n",
-        "2 of Diamonds\n",
-        "3 of Diamonds\n",
-        "4 of Diamonds\n",
-        "5 of Diamonds\n",
-        "6 of Diamonds\n",
-        "7 of Diamonds\n",
-        "8 of Diamonds\n",
-        "9 of Diamonds\n",
-        "10 of Diamonds\n",
-        "Jack of Diamonds\n",
-        "Queen of Diamonds\n",
-        "King of Diamonds\n",
-        "Ace of Clubs\n",
-        "2 of Clubs\n",
-        "3 of Clubs\n",
-        "4 of Clubs\n",
-        "5 of Clubs\n",
-        "6 of Clubs\n",
-        "7 of Clubs\n",
-        "8 of Clubs\n",
-        "9 of Clubs\n",
-        "10 of Clubs\n",
-        "Jack of Clubs\n",
-        "Queen of Clubs\n",
-        "King of Clubs\n",
-    }
-    int *card_numbers[] = {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-        14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-        27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 29,
-        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
-    }
+static int deck_numbers[] = {
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+	14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+	27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 29,
+	40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
+};
+
+enum state {
+	DISABLED = 0,
+	RESET = 1,
+	SHUFFLED = 2,
 };
 
 struct game_state {
-    struct deck game_deck;
-    int state = 0;
-    int dealer_score = 0;
-    int player_score = 0;
+    enum state current_state;
+    int dealer_score;
+    int player_score;
 };
 
 
@@ -125,18 +129,26 @@ static ssize_t device_write(struct file *file, const char __user *buff, size_t l
     return -EPERM;
 }
 
-static void shuffle(struct card_deck *deck){
+static void shuffle(int card_numbers[]){
     size_t i, j;
     int tmp;
-    unsigned int rand_gen = prandam_u32();
+    unsigned int rand_gen = prandom_u32();
+
 
     for (i = 0; i < 52; i++) {
         j = ((i * 7) * rand_gen);
         j %= 52;
-        tmp = deck.card_numbers[j];
-        deck.card_numbers[j] = deck.card_numbers[i];
-        deck.card_numbers[i] = tmp;
+        tmp = card_numbers[j];
+        card_numbers[j] = card_numbers[i];
+        card_numbers[i] = tmp;
     }
+}
+
+static void reset(int card_numbers[]){
+	int i;
+	for (i = 0; i < 52; i++){
+		card_numbers[i] = (i + 1);
+	}
 }
 
 static int __init blackjack_init(void) {
